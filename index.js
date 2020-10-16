b1194de2b031d45a4314b7019ecb2c034849e4f2
Shun Yamada <shyamady@Shuns-MacBook-Pro.local>
2020-10-15 19:41:27 -0700

    add slack webhook

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
+
 var Twitter = require('twitter')
+var { IncomingWebhook } = require('@slack/webhook');
 
 var client = new Twitter({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
@@ -7,6 +9,10 @@ var client = new Twitter({
   access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
+var twitter_url = "https://twitter.com"
+
+var url = process.env.SLACK_WEBHOOK_URL;
+var webhook = new IncomingWebhook(url);
 
 var current_time = new Date().getTime()
 
@@ -14,8 +20,8 @@ client.get('search/tweets', {q: 'hello', count: 100000, result_type: "recent", e
   if(!error) {
     var { statuses } = tweets
     statuses.forEach(tweet => {
-      var { created_at, user } = tweet
-      var { followers_count, retweet_count, favorite_count } = user
+      var { created_at, id, user, text } = tweet
+      var { followers_count, favorite_count, name, retweet_count, screen_name } = user
 
       // NOTE: created_at should be within 1 hour
       var tweet_time = new Date(created_at).getTime()
@@ -23,7 +29,19 @@ client.get('search/tweets', {q: 'hello', count: 100000, result_type: "recent", e
       if(time_diff > 1) return
 
       // NOTE: configure tweets on your customized settings
-      if(followers_count < 10000 || retweet_count < 0 || favorite_count < 0) return
+      if(followers_count < 10000 || favorite_count < 0 || retweet_count < 0) return
+
+      var attachments = {
+        attachments: [
+          {
+            author_name: user.name,
+            author_link: `${twitter_url}/${user.slug}`,
+            text: `${name} tweeted a tweet about 'hello'\n${text}\nURL: ${twitter_url}/${screen_name}/status/${id}`,
+            color: '#00acee',
+          },
+        ],
+      }
+      webhook.send(attachments);
     })
   }
 });
